1b968254b0ee15dc7835411d8864a1b761b70570
Rick Branson <rick@diodeware.com>
2009-08-25 13:35:48 -0500

    Prettied up; Added question validations;

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+
+  validates_presence_of :title
+  validates_presence_of :body
 end
--- test/unit/question_test.rb
+++ test/unit/question_test.rb
@@ -1,8 +1,21 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :questions
+  
+  def test_question_should_have_non_empty_title
+    a = questions(:two)
+    assert a.save
+    
+    a.title = ""
+    assert !a.save
+  end
+  
+  def test_question_should_have_non_empty_body
+    a = questions(:two)
+    assert a.save
+    
+    a.body = ""
+    assert !a.save    
   end
 end
